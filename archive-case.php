711e923b2c027528f52280bdf64bf630fd71ba4c
Mikael Holgersson <mikael.holgersson@tdc.se>
2017-01-11 11:36:54 +0100

    Lagt till uppdelning av case status: Komplettering från beställare behövs Nya Beställningar Aktiva Beställningar Slutförda Beställningar
    Signed-off-by: Mikael Holgersson <mikael.holgersson@tdc.se>

--- archive-case.php
+++ archive-case.php
@@ -4,9 +4,37 @@
 		<div class="flex-box flex-box-big">
 			<img src="<?php bloginfo('template_url');?>/img/logo2.png" alt="loggo">
 			<fieldset>
+			<h3>Kompletering från beställare behövs</h3>
+			<?php if(have_posts()): while(have_posts()): the_post(); 
+				if (get_field('case_status') == 'Ny'){?>
+<!--					if (get_field('case_status') == 'Nyskapad'){
+						<script>window.location.href = "http://infox/best/";</script>
+						exit;
+					}?> -->
+					<h1 class="flex-title"><a href=<?php the_permalink(); ?>><?php the_field('kundnamn'); ?> - <?php the_title(); ?> </a></h1>
+				<?php }	?>
+
+			<?php endwhile; ?>
+			<?php endif; ?>
+			</fieldset>
+			<fieldset>
+			<h3>Nya Beställningar</h3>
+			<?php if(have_posts()): while(have_posts()): the_post(); 
+				if (get_field('case_status') == 'DCO har mottagit beställning'){?>
+<!--					if (get_field('case_status') == 'Nyskapad'){
+						<script>window.location.href = "http://infox/best/";</script>
+						exit;
+					}?> -->
+					<h1 class="flex-title"><a href=<?php the_permalink(); ?>><?php the_field('kundnamn'); ?> - <?php the_title(); ?> </a></h1>
+				<?php }	?>
+
+			<?php endwhile; ?>
+			<?php endif; ?>
+			</fieldset>
+			<fieldset>
 			<h3>Aktiva Beställningar</h3>
 			<?php if(have_posts()): while(have_posts()): the_post(); 
-				if (get_field('case_status') != 'Klar'){?>
+				if (get_field('case_status') != 'Klar' AND get_field('case_status') != 'DCO har mottagit beställning' AND get_field('case_status') != 'Ny'){?>
 <!--					if (get_field('case_status') == 'Nyskapad'){
 						<script>window.location.href = "http://infox/best/";</script>
 						exit;
