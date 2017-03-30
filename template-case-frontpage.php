9fbf3a1decf9e093523ab8e87380f75ebc055f27
Mikael Holgersson <mikael.holgersson@tdc.se>
2017-03-30 15:44:30 +0200

    updaterat template så att info på förstasidan innehåller mer data enligt önskemål från Anders

--- template-case-frontpage.php
+++ template-case-frontpage.php
@@ -17,7 +17,16 @@
 				<p>( Delorder saknas )</p>
 				<h3>Komplettering från beställare behövs!!</h3>
 				<?php if($query->have_posts()): while($query->have_posts()): $query->the_post(); ?>
-					<h1 class="flex-title"><a href=<?php the_permalink(); ?>><?php the_field('kundnamn'); ?> - <?php the_title(); ?> </a></h1>
+				<div class="divTable">
+					 <div class="headRow">
+						<div class="divCell50" align="center"><b>Beställningar</b></div>
+						<div  class="divCell50"><b>Order-status</b></div>
+					 </div>
+					<div class="divRow">
+						<div class="divCell50"><a href=<?php the_permalink(); ?>><?php the_field('kundnamn'); ?> - <?php the_title(); ?> </a></div>
+						<div class="divCell50">Delorder saknas</div>
+					</div>
+				</div>
 				<?php endwhile; ?>
 				<?php endif; ?>
 				</fieldset>
@@ -34,10 +43,21 @@
 			$query = new WP_Query( $args );
 			?>
 			<h3>Nya Beställningar</h3>
-			<?php if($query->have_posts()): while($query->have_posts()): $query->the_post(); ?>
-				<h1 class="flex-title"><a href=<?php the_permalink(); ?>><?php the_field('kundnamn'); ?> - <?php the_title(); ?> </a>: <?php the_field('casetyp'); ?> </h1>
-			<?php endwhile; ?>
-			<?php endif; ?>
+				<div class="divTable">
+					 <div class="headRow">
+						<div class="divCell50" align="center"><b>Beställningar</b></div>
+						<div  class="divCell25"><b>Order-typ</b></div>
+						<div  class="divCell25"><b>Skapad</b></div>
+					 </div>
+					<?php if($query->have_posts()): while($query->have_posts()): $query->the_post(); ?>
+						<div class="divRow">
+							<div class="divCell50"><a href=<?php the_permalink(); ?>><?php the_field('kundnamn'); ?> - <?php the_title(); ?> </a></div>
+							<div class="divCell25"><?php the_field('casetyp'); ?></div>
+							<div class="divCell25"><?php the_date('Y-m-d'); ?></div>
+						</div>
+					<?php endwhile; ?>
+					<?php endif; ?>
+				</div>
 			</fieldset>
 
 
@@ -49,12 +69,21 @@
 			$query = new WP_Query( $args );
 			?>
 			<h3>Aktiva Beställningar</h3>
-			<?php if($query->have_posts()): while($query->have_posts()): $query->the_post(); ?>
-				<?php if (get_field('case_status') != 'Klar' AND get_field('case_status') != 'DCO har mottagit beställning' AND get_field('case_status') != 'Ny'){?>
-					<h1 class="flex-title"><a href=<?php the_permalink(); ?>><?php the_field('kundnamn'); ?> - <?php the_title(); ?> </a></h1>
-				<?php }	?>
-			<?php endwhile; ?>
-			<?php endif; ?>
+			<div class="divTable">
+				<div class="headRow">
+					<div class="divCell50" align="center"><b>Beställningar</b></div>
+					<div  class="divCell50"><b>Order-status</b></div>
+				</div>
+				<?php if($query->have_posts()): while($query->have_posts()): $query->the_post(); ?>
+					<?php if (get_field('case_status') != 'Klar' AND get_field('case_status') != 'DCO har mottagit beställning' AND get_field('case_status') != 'Ny'){?>
+						<div class="divRow">
+							<div class="divCell50"><a href=<?php the_permalink(); ?>><?php the_field('kundnamn'); ?> - <?php the_title(); ?> </a></div>
+							<div class="divCell50"><?php the_field('case_status'); ?></div>
+						</div>
+					<?php }	?>
+				<?php endwhile; ?>
+				<?php endif; ?>
+			</div>
 			</fieldset>
 
 
